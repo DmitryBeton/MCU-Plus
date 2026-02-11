5b10eb671e3e683114d8283aeaf502b2562e26d7
Beton <dima41768@icloud.com>
2026-02-11 22:36:31 +0300

    Initial commit

---
+++ MGPUPlus/MGPUPlusApp.swift
@@ -0,0 +1,21 @@
+//
+//  MGPUPlusApp.swift
+//  MGPUPlus
+//
+//  Created by Дмитрий Чалов on 11.02.2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct MGPUPlusApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
